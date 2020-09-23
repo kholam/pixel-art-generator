9211b868ea83ef4af74c0e2397f74b0f61351346
Guillermo Villalta <mexcelus@gmail.com>
2020-09-22 23:47:19 -0600

    :sparkles: color picker component
    color picker component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import useStyles from './App.styles';
 import Grid from './components/Grid';
+import ColorPicker from './components/ColorPicker'
 
 function App() {
   const classes = useStyles();
   return (
     <div className={classes.app}>
+      <ColorPicker />
       <Grid />
     </div>
   );
---
+++ src/components/ColorPicker.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ColorPicker = () => {
+    return <input type="color" value="#f6b73c"/> 
+};
+
+export default ColorPicker;
