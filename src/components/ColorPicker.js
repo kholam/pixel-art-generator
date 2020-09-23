29cac79641a11c1680c4b2583bde4906297a55f7
Guillermo Villalta <mexcelus@gmail.com>
2020-09-23 00:10:19 -0600

    style

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import useStyles from './ColorPicker.styles'
 
 const ColorPicker = () => {
-    return <input type="color" value="#f6b73c"/> 
+    const classes = useStyles();
+    return (
+        <div className={classes.tools}>
+            <div className={classes.colorPicker}>
+                <label>Color:</label>
+                <input type="color" value="#f6b73c" /> 
+            </div>
+        </div>
+    )
 };
 
 export default ColorPicker;
---
+++ src/components/ColorPicker.styles.js
@@ -0,0 +1,16 @@
+import { createUseStyles } from 'react-jss';
+
+export default createUseStyles({
+  tools: {
+    position: 'absolute',
+    width: '100%',
+    height: '4em',
+    top: 0,
+    background: '#cecece',
+  },
+  colorPicker: {
+    position: 'absolute',
+    top: '1em',
+    left: '2em',
+  }
+});
